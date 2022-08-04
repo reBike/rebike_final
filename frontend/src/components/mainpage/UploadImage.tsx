eee0fcc838a4a2a40e1d675935b0778a232641b7
Ryokuman <ryokuman21@gmail.com>
2022-08-04 22:38:13 +0900

    finished

--- frontend/src/components/mainpage/UploadImage.tsx
+++ frontend/src/components/mainpage/UploadImage.tsx
@@ -11,205 +11,220 @@ import { save_ID } from "../../actions/ImgIDActions";
 import lottie from "lottie-web";
 
 const LoadingLottie = () => {
-  //lottie
-  const element = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    lottie.loadAnimation({
-      container: element.current as HTMLDivElement,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: require("../../images/LottieLoading.json"),
-    });
-  }, []);
-  return <Box ref={element} style={{ marginTop: 60, height: 230 }}></Box>;
+    //lottie
+    const element = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: element.current as HTMLDivElement,
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            animationData: require("../../images/LottieLoading.json"),
+        });
+    }, []);
+    return <Box ref={element} style={{ marginTop: 60, height: 230 }}></Box>;
 };
 
 function UploadImage() {
-  const [isImg, setIsImg] = useState(null);
-  const [urlImg, setUrlImg] = useState("");
-  const [respondImg, setRespondImg] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [checked, setChecked] = useState(false);
+    const [isImg, setIsImg] = useState(null);
+    const [urlImg, setUrlImg] = useState("");
+    const [respondImg, setRespondImg] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [checked, setChecked] = useState(false);
 
-  const navigate = useNavigate();
-  const userIdtoRedux = ReduxModule().decodeInfo?.id;
+    const navigate = useNavigate();
+    const userIdtoRedux = ReduxModule().decodeInfo?.id;
 
-  const dispatch = useDispatch();
-  const what: any = getAccess();
+    const dispatch = useDispatch();
+    const what: any = getAccess();
 
-  const resizeFile = (file: Blob) =>
-    new Promise((resolve) => {
-      Resizer.imageFileResizer(
-        file,
-        500, // max width
-        250, // max height
-        "JPEG",
-        513, // min width
-        0, // min height
-        (uri) => {
-          resolve(uri);
-        },
-        "file" // 저장 형식
-      );
-    });
-
-  const onChangeImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    try {
-      const file: any =
-        event.target.files instanceof FileList ? event.target.files[0] : null;
-
-      setRespondImg(file);
-
-      const img: any = await resizeFile(file);
-      setIsImg(img);
-      setUrlImg(URL.createObjectURL(img));
-    } catch (err) {
-    }
-  };
-
-  const sendImage: () => Promise<any> = async () => {
-    const trashFormData = new FormData();
-    trashFormData.append("filename", respondImg as any);
-
-    var task_id = "";
-
-    await Api.post(
-      `/trash/users/${userIdtoRedux}/results/tasks`,
-      trashFormData,
-      {
-        headers: {
-          Authorization: `${what.value}`,
-        },
-      }
-    )
-      .then((res) => {
-        task_id = res.data.task_id;
-        setChecked(true);
-      })
-      .catch((error) => {
-        navigate(`/errorpage`);
-      });
-
-    if (task_id !== "") {
-      const getAnswer = () => {
-        Api.get(`/trash/users/${userIdtoRedux}/results/tasks/${task_id}`, {
-          headers: {
-            Authorization: `${what.value}`,
-          },
-        })
-          .then((res) => {
-            dispatch(save_ID(res.data.image_id));
-
-            if (res.status === 200) {
-              //제대로 들어갔을 때
-              navigate(`/howtopage`, {
-                state: {
-                  challenge_id: res.data.challenge_id,
-                  challenge_content: res.data.challenge_content,
+    const resizeFile = (file: Blob) =>
+        new Promise((resolve) => {
+            Resizer.imageFileResizer(
+                file,
+                500, // max width
+                250, // max height
+                "JPEG",
+                513, // min width
+                0, // min height
+                (uri) => {
+                    resolve(uri);
                 },
-              });
-              clearInterval(timer);
+                "file" // 저장 형식
+            );
+        });
+
+    const onChangeImage = async (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        try {
+            const file: any =
+                event.target.files instanceof FileList
+                    ? event.target.files[0]
+                    : null;
+
+            setRespondImg(file);
+
+            const img: any = await resizeFile(file);
+            setIsImg(img);
+            setUrlImg(URL.createObjectURL(img));
+        } catch (err) {}
+    };
+
+    const sendImage: () => Promise<any> = async () => {
+        const trashFormData = new FormData();
+        trashFormData.append("filename", respondImg as any);
+
+        var task_id = "";
+
+        await Api.post(
+            `/trash/users/${userIdtoRedux}/results/tasks`,
+            trashFormData,
+            {
+                headers: {
+                    Authorization: `${what.value}`,
+                },
             }
-          })
-          .catch((error) => {
-            // 분류 안됐을때
-            clearInterval(timer);
-            navigate("/errorpage");
-          });
-      };
+        )
+            .then((res) => {
+                task_id = res.data.task_id;
+                setChecked(true);
+            })
+            .catch((error) => {
+                navigate(`/errorpage`);
+            });
 
-      const timer = setInterval(getAnswer, 2000);
-      return () => clearInterval(timer);
-    } // if
-  };
+        if (task_id !== "") {
+            const getAnswer = () => {
+                Api.get(
+                    `/trash/users/${userIdtoRedux}/results/tasks/${task_id}`,
+                    {
+                        headers: {
+                            Authorization: `${what.value}`,
+                        },
+                    }
+                )
+                    .then((res) => {
+                        dispatch(save_ID(res.data.image_id));
 
-  const onClickImgResult = () => {
-    if (isImg === null) return alert("no image");
-    else {
-      setLoading(true);
-      sendImage();
-    }
-  };
+                        if (res.status === 200) {
+                            //제대로 들어갔을 때
+                            navigate(`/howtopage`, {
+                                state: {
+                                    challenge_id: res.data.challenge_id,
+                                    challenge_content:
+                                        res.data.challenge_content,
+                                },
+                            });
+                            clearInterval(timer);
+                        }
+                    })
+                    .catch((error) => {
+                        // 분류 안됐을때
+                        clearInterval(timer);
+                        navigate("/errorpage");
+                    });
+            };
 
-  if (loading)
+            const timer = setInterval(getAnswer, 2000);
+            return () => clearInterval(timer);
+        } // if
+    };
+
+    const onClickImgResult = () => {
+        if (isImg === null) return alert("no image");
+        else {
+            setLoading(true);
+            sendImage();
+        }
+    };
+
+    if (loading)
+        return (
+            <div>
+                <LoadingLottie />
+                <Typography
+                    sx={{
+                        fontFamily: "Nanum1",
+                        mt: 2,
+                        fontSize: 20,
+                        fontWeight: "bold",
+                    }}
+                >
+                    결과 분석 중 ..
+                </Typography>
+            </div>
+        );
+
     return (
-      <div>
-        <LoadingLottie />
-        <Typography
-          sx={{ fontFamily: "Nanum1", mt: 2, fontSize: 20, fontWeight: "bold" }}
-        >
-          결과 분석 중 ..
-        </Typography>
-      </div>
-    );
-
-  return (
-    <Box>
-      <form>
-        <Button
-          variant="outlined"
-          sx={{
-            borderColor: "white",
-            borderRadius: 3,
-            boxShadow: "1px 3px 3px #B0B09A",
-            backgroundColor: "white",
-            width: 600,
-            height: 300,
-            mt: 5,
-            "&:hover": {
-              backgroundColor: "#D4D4D4",
-              borderColor: "#F7F8E9",
-            },
-            "& .MuiTouchRipple-root span": {
-              backgroundColor: "#8F704E",
-              opacity: 0.3,
-            },
-          }}
-          component="label"
-        >
-          <img src={urlImg}></img>
-          <input
-            type="file"
-            hidden
-            required
-            onChange={(e) => onChangeImage(e)}
-          />
-          {isImg ? null : (
-            <Box>
-              {" "}
-              <CloudUploadIcon sx={{ color: "#B8B8B8" }} fontSize="large" />
-              <Typography sx={{ color: "#759F98" }}>
-                {" "}
-                Upload your image!
-              </Typography>
-            </Box>
-          )}
-        </Button>
         <Box>
-          <Button
-            onClick={onClickImgResult}
-            variant="contained"
-            sx={{
-              "&:hover": {
-                backgroundColor: "#51523E",
-              },
-              mt: 2,
-              width: 80,
-              height: 30,
-              fontWeight: "bold",
-              fontSize: 12,
-              mb: 2,
-              color: "white",
-              backgroundColor: "#737458",
-            }}
-          >
-            결과보기
-          </Button>
+            <form>
+                <Button
+                    variant="outlined"
+                    sx={{
+                        borderColor: "white",
+                        borderRadius: 3,
+                        boxShadow: "1px 3px 3px #B0B09A",
+                        backgroundColor: "white",
+                        width: 600,
+                        height: 300,
+                        mt: 5,
+                        "&:hover": {
+                            backgroundColor: "#D4D4D4",
+                            borderColor: "#F7F8E9",
+                        },
+                        "& .MuiTouchRipple-root span": {
+                            backgroundColor: "#8F704E",
+                            opacity: 0.3,
+                        },
+                    }}
+                    component="label"
+                >
+                    <img src={urlImg}></img>
+                    <input
+                        type="file"
+                        hidden
+                        required
+                        onChange={(e) => onChangeImage(e)}
+                    />
+                    {isImg ? null : (
+                        <Box>
+                            {" "}
+                            <CloudUploadIcon
+                                sx={{ color: "#B8B8B8" }}
+                                fontSize="large"
+                            />
+                            <Typography sx={{ color: "#759F98" }}>
+                                {" "}
+                                Upload your image!
+                            </Typography>
+                        </Box>
+                    )}
+                </Button>
+                <Box>
+                    <Button
+                        onClick={onClickImgResult}
+                        variant="contained"
+                        sx={{
+                            "&:hover": {
+                                backgroundColor: "#51523E",
+                            },
+                            mt: 2,
+                            width: 80,
+                            height: 30,
+                            fontWeight: "bold",
+                            fontSize: 12,
+                            mb: 2,
+                            color: "white",
+                            backgroundColor: "#737458",
+                        }}
+                    >
+                        결과보기
+                    </Button>
+                </Box>
+            </form>
         </Box>
-      </form>
-    </Box>
-  );
+    );
 }
 
 export default UploadImage;
--- frontend/src/utils/customApi.tsx
+++ frontend/src/utils/customApi.tsx
@@ -2,9 +2,9 @@ import axios from "axios";
 import { API_BASE_URL } from "./constants";
 
 const Api = axios.create({
-  baseURL: `${API_BASE_URL}`,
-  timeout: 10000,
-  params: {},
+    baseURL: `${API_BASE_URL}`,
+    timeout: 10000,
+    params: {},
 });
 
 export default Api;
